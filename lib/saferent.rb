c53bbe7f131c9cb020917e41cd3eded885d4d3b0
Hasham Malik <hasham2@gmail.com>
2012-12-27 20:33:23 +0500

    basic client class

--- lib/saferent.rb
+++ lib/saferent.rb
@@ -1,11 +1,23 @@
 require "saferent/version"
 require "configatron"
+require 'httparty'
 
 module Saferent
   class Client
-    def initialize
+    include HTTParty
+    base_uri 'consumers.residentscreening.net:443'
 
+    def initialize(options = {})
+      if options.key?(:account) && options.key?(:user_name) && options.key?(:password) && options.key?(:originator_id)
+        configatron.account       = options[:account]
+        configatron.user_name     = options[:user_name]
+        configatron.password      = options[:password]
+        configatron.originator_id = options[:originator_id]
+      else
+        raise ArgumentError
+      end
     end
+
   end
-  # Your code goes here...
+
 end
