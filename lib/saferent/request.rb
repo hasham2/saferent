ee4c1b28268aac50fe11ebc157a871b271106222
Hasham Malik <hasham2@gmail.com>
2013-01-05 14:04:41 +0500

    working on request

--- lib/saferent/request.rb
+++ lib/saferent/request.rb
@@ -1,5 +1,14 @@
 module Saferent
   class Request
 
+    def initialize
+      #TBD
+    end
+
+    def send(client)
+      raise ArgumentError unless client.kind_of? Saferent::Client
+      full_response = client.class.post('/b2b/demits.aspx', :body => {:XMLPost => "--- xml string ---"})
+      Saferent::Response.new(full_response["ApplicantScreening"]["Response"])
+    end
   end
 end
